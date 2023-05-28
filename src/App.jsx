afb72f582c70d5c0c4e6301a0abb66b3feed187b
rostyslaveMuha <rostyslavmushynskyi@gmail.com>
2023-05-28 15:19:43 +0300

    Add Local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import { nanoid } from "nanoid";
 import { Container, Typography } from "@mui/material";
 import Filter from "./components/Filter/Filter";
@@ -6,13 +7,21 @@ import ContactForm from "./components/ContactForm/ContactForm";
 import ContactsList from "./components/ContactList/ContactList";
 
 function App() {
-  const [contacts, setContacts] = useState([
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]);
+  const [contacts, setContacts] = useState(initContactsState);
   const [filter, setFilter] = useState("");
+
+  function initContactsState() {
+    const contactsLocalStorage = localStorage.getItem("contacts");
+    if (contactsLocalStorage) {
+      return JSON.parse(contactsLocalStorage);
+    } else {
+      return [];
+    }
+  }
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
 
   const addContact = (name, number) => {
     const isDuplicateName = contacts.some(
